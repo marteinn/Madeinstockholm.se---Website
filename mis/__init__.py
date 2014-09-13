ed5260b9fa1444e3d97bd9c426b3b3f5f9a8feb9
Martin Sandström <martin@marteinn.se>
2014-09-13 22:12:29 +0200

    Bumped version

---
+++ mis/__init__.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+mis
+----------
+
+This is the MadeInStockholm flask backend.
+"""
+
+
+__title__ = 'mis'
+__version__ = '1.1'
+__build__ = 11
+__author__ = 'Martin Sandström'
+__license__ = 'Russian Dance HB License'
+__copyright__ = 'Copyright 2014 Russian Dance HB'
